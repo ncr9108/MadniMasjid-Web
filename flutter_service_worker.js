e8ced9452f39b1e4ada46e71a045e8070d04670c
Nisarg Rotliwala <nisarg.rotliwala99@gmail.com>
2021-11-02 18:34:07 +0530

    Set

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,9 +22,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "caa7b55d73a73d630bc605345287b39b",
-"/": "caa7b55d73a73d630bc605345287b39b",
-"main.dart.js": "127837f05adabd76d1aa248a63a067d8",
+"index.html": "567eab2cf80d2f6f709b5334b68fc17e",
+"/": "567eab2cf80d2f6f709b5334b68fc17e",
+"main.dart.js": "660fd9f1d773cee5bb9af3aca3255566",
 "manifest.json": "d9037c6231a4835eb2c02404eeeae16b",
 "version.json": "8c3452c2b7884b986836c09adba3f7a5"
 };
